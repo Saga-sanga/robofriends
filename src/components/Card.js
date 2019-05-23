12093d8d5ff5eb5fa08551bfc51035aa197051b5
Reckson <vulcan248@gmail.com>
2019-05-23 16:02:06 +0530

    refactored code and added scroll

--- src/components/Card.js
+++ src/components/Card.js
@@ -2,11 +2,12 @@ import React from 'react';
 
 const Card = ({ name, email, id }) => {
 	return (
-		<div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
+		<div className='tc bg-light-green dib br3 pa1 ma2 grow bw2 shadow-5'>
 			<div>
 				<img alt='robots' src={`https://robohash.org/${id}?200x200`} />
 				<h2>{name}</h2>
 				<p>{email}</p>
+				<p>Khaiyaa!</p>
 			</div>
 		</div>
 		);
