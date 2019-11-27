6d910d37812ccc1cf7f06f555b8139dbd3d35b9f
Reckson Khiangte <vulcan248@gmail.com>
2019-11-27 17:30:33 +0530

    Updates

---
+++ precache-manifest.e1e8cd3167516c910e2050d64e89f229.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb75599bd13b6990ac2deba84cb9cff2",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "26dafc4245b14a37f593",
+    "url": "/robofriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "7a5992e263bc4d554b47",
+    "url": "/robofriends/static/css/main.089a8a4f.chunk.css"
+  },
+  {
+    "revision": "26dafc4245b14a37f593",
+    "url": "/robofriends/static/js/2.1f39b60e.chunk.js"
+  },
+  {
+    "revision": "7a5992e263bc4d554b47",
+    "url": "/robofriends/static/js/main.4d976dc6.chunk.js"
+  },
+  {
+    "revision": "7b4f849e958ac4107787",
+    "url": "/robofriends/static/js/runtime-main.a1071882.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
